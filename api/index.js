80653a8285c7871bf25a60d420c17feece520a98
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2025-01-07 14:14:33 +0200

    upload home route

--- api/index.js
+++ api/index.js
@@ -6,11 +6,11 @@ const port = 6000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/homepage", (req, res) => {
-  res.json("Hello from homepage");
+app.get("/", (req, res) => {
+  res.json("hello this first page from server");
 });
 
-app.get("users", (req, res) => {
+app.get("/users", (req, res) => {
   const sql = "SELECT * FROM users";
 });
 
