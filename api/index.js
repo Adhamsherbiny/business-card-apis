ea7a8b8f9b136648a1a878be6239df7a19dc10a6
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2025-01-07 16:17:06 +0200

    edit use route delete bctypt

--- api/index.js
+++ api/index.js
@@ -28,6 +28,8 @@ app.post("/create_new_user", (req, res) => {
   const email = req.body.email;
   const phone = req.body.phone;
   const anotherPhone = req.body.anotherPhone;
+  const values = [username, password, role, email, phone, anotherPhone];
+
   connection.query(
     "SELECT * FROM users where username = ?",
     [username],
@@ -35,16 +37,14 @@ app.post("/create_new_user", (req, res) => {
       if (result.length > 2) {
         res.json("user is already exist");
       } else {
-        const sql = `INSERT INTO users(username, password , role , email , phone ,  another_phone ) VALUES(?)`;
-        const values = [username, password, role, email, phone, anotherPhone];
-        bcrypt.hash(password, 10, (err, hash) => {
-          connection.query(sql, [values], (err, result) => {
-            if (err) {
-              console.log(err);
-            } else {
-              res.json(result);
-            }
-          });
+        const sql =
+          "INSERT INTO users (username, password, role, email, phone, another_phone) VALUES (?)";
+        connection.query(sql, [values], (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.json("user created");
+          }
         });
       }
     }
