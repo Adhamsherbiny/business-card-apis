317c491038e323d78e8076c95e36b83eeddac15f
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2025-01-07 15:32:29 +0200

    create users route'add new user'

--- api/index.js
+++ api/index.js
@@ -21,6 +21,27 @@ app.get("/users", (req, res) => {
   });
 });
 
+app.post("/users", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const role = req.body.role;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const anotherPhone = req.body.anotherPhone;
+  const sql = `INSERT INTO users(username, password , role , email , phone ,  another_phone ) VALUES(?)`;
+  const passwordHash = password.bcrypt();
+  const values = [username, passwordHash, role, email, phone, anotherPhone];
+  password.hash(10, (err, hash) => {
+    connection.query(sql, [values], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(result);
+      }
+    });
+  });
+});
+
 app.listen(port, () => {
   console.log(`app listenning port on port ${port}`);
 });
