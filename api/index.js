d271d567e86df54efcedc1940e6501ab137d4005
adhamsherbiny <adhamsherbiny.business@hotmail.com>
2025-01-07 17:10:03 +0200

    update new routes

--- api/index.js
+++ api/index.js
@@ -61,6 +61,79 @@ app.post("/create_new_user", (req, res) => {
     }
   );
 });
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  connection.query(
+    "SELECT * FROM users where username = ?",
+    [username],
+    async (err, result) => {
+      if (result.length > 0) {
+        const isMatch = await bcrypt.compare(password, result[0].password);
+        if (isMatch) {
+          res.json(result);
+        } else {
+          res.json("password is not correct");
+        }
+      } else {
+        res.json("user is not exist");
+      }
+    }
+  );
+});
+
+app.put("/update_user", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const role = req.body.role;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const anotherPhone = req.body.anotherPhone;
+
+  connection.query(
+    "SELECT * FROM users where username = ?",
+    [username],
+    async (err, result) => {
+      if (result.length > 0) {
+        const hasedPassword = await bcrypt.hash(password, 10);
+
+        const values = [
+          username,
+          hasedPassword,
+          role,
+          email,
+          phone,
+          anotherPhone,
+        ];
+
+        const sql =
+          "UPDATE users SET username = ?, password = ?, role = ?, email = ?, phone = ?, another_phone = ? WHERE username = ?";
+        connection.query(sql, [...values, username], (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.json("user updated");
+          }
+        });
+      } else {
+        res.json("user is not exist");
+      }
+    }
+  );
+});
+
+app.delete("/delete_user", (req, res) => {
+  const username = req.body.username;
+  const sql = "DELETE FROM users WHERE username = ?";
+  connection.query(sql, [username], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json("user deleted");
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`app listenning port on port ${port}`);
